1de66ccb932b29b85cb5b265ad33ceec0734368b
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2019-03-04 22:28:08 -0800

    change

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -50,7 +50,7 @@ class Login extends Component {
 
     return (
       <div className="login">
-        <div className="container">
+        <div className="container"> 
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
