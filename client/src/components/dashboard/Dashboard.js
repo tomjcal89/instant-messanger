23d6373f638b4093bfc48cdd8e9b7b6e06deb78a
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2019-03-04 22:06:32 -0800

    added some layout to the chatroom

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,16 +4,13 @@ import { connect } from 'react-redux';
 // import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 
-
-
-
 class Dashboard extends Component {
   componentDidMount() {
     // this.props.getCurrentProfile();
-  } 
- 
+  }
+
   onDeleteClick(e) {
-    this.props.deleteAccount(); 
+    this.props.deleteAccount();
   }
 
   render() {
@@ -25,36 +22,83 @@ class Dashboard extends Component {
     if (user === null || loading) {
       chatroom = <Spinner />;
     } else {
-        chatroom = (
-          <div>
-            <p className="lead text-muted">Welcome {user.name}!</p>
-          </div>
-        );
+      chatroom = (
+        <div>
+          <p className="lead text-muted">Welcome {user.name}!</p>
+        </div>
+      );
     }
 
     return (
       <div className="dashboard">
         <div className="container">
+          <div className="row1">{chatroom}</div>
+
+          {/* chatroom box and text input starts here */}
           <div className="row">
-            <div className="col-md-12">
-              <h1 className="display-4">Chatroom</h1>
-              {chatroom}
+            <div className="col-md-10">
+              <h1>Chatroom</h1>
+              <div class="jumbotron col-10">
+                <h3>Chat will take place in here</h3>
+              </div>
+              <div className="row">
+              <div class="form-group col-8">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
+              </div>
+              <input class="btn btn-primary " type="submit" value="Submit"></input>
+            </div>
+            </div>
+            
+            {/* user div starts here to list users */}
+            <div className="col-md-2">
+              <h1>Users</h1>
+              <ul class="list-group">
+                <div className="userDiv">
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                  <li className="list-group">{user.name}
+                  </li>
+                  <hr />
+                </div>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-      
-    ); 
+
+    );
   }
 }
 
 Dashboard.propTypes = {
   auth: PropTypes.object.isRequired,
-  // profile: PropTypes.object.isRequired
+
 };
 
 const mapStateToProps = state => ({
-  // profile: state.profile,
+
   auth: state.auth
 });
 
